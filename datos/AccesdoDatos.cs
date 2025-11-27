e984f881bfde54cf33fc386d0d77b909306c2512
unknown <les_17_10_94@hotmail.com>
2025-11-27 19:47:19 -0300

    clases y accesoBD

---
+++ datos/AccesdoDatos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector => lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("Server=.\\SQLEXPRESS; Database=NegocioDB; Integrated Security=True");
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+        }
+
+        public void AbrirConexion()
+        {
+            if (conexion.State != ConnectionState.Open)
+                conexion.Open();
+        }
+    }
+}
